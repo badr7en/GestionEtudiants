f37dcdcc20e0c91f56ab273f921b6bd9901dddd3
badr <badr@192.168.56.1>
2022-04-15 11:45:59 +0000

    la méthode toString

--- GestionEtudiants/src/aiac/gi18cours/java/Etudiant.java
+++ GestionEtudiants/src/aiac/gi18cours/java/Etudiant.java
@@ -4,10 +4,13 @@ public class Etudiant {
 	private int id;
 	private String nom;
 	private double note;
-	public Etudiant(int A, String name) {
+	public Etudiant(int A, String name){
 		this.id=A;
 		this.note=10.0;
 		this.nom=name;
-	}
+		}
+	public String toString() {
+		return "("+this.nom + ":"+ this.note+")";
 
+}
 }
