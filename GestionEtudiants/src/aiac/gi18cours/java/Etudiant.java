a26d65e02d7ca2e6d15e00e83b1467129ee5e771
badr <badr@192.168.56.1>
2022-04-15 11:38:04 +0000

    constructeur

--- GestionEtudiants/src/aiac/gi18cours/java/Etudiant.java
+++ GestionEtudiants/src/aiac/gi18cours/java/Etudiant.java
@@ -4,5 +4,10 @@ public class Etudiant {
 	private int id;
 	private String nom;
 	private double note;
+	public Etudiant(int A, String name) {
+		this.id=A;
+		this.note=10.0;
+		this.nom=name;
+	}
 
 }
